ff13f81d5ba9673910e60cfcd8ee08085c42096e
philanderson888 <philanderson888@hotmail.com>
2025-02-28 11:11:02 +0000

    explicitly check and update node to latest

--- update-node-to-latest.sh
+++ update-node-to-latest.sh
@@ -10,4 +10,15 @@ echo "npm -v"
 npm -v
 
 echo " "
-
+echo "update node npm to latest"
+echo " "
+echo "npm install -g npm@latest"
+npm install -g npm@latest
+echo " "
+echo "check versions now"
+echo " "
+echo "node -v"
+node -v
+echo "npm -v"
+npm -v
+echo " "
